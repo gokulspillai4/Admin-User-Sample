8a033a37cdcc22f61d8000a2421054d38dc74c48
Gokul S Pillai <gokulspillai4@gmail.com>
2021-09-20 13:03:04 +0530

    design complete

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login' });
 });
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Login' });
+});
+
 
 module.exports = router;
